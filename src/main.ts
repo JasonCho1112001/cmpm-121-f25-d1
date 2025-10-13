9589d94db07ac237abe605e991a93926d21db17f
JasonCho1112001 <jcho111@ucsc.edu>
2025-10-13 19:31:05 +0000

    Step 9: Data Driven design, referencing availableItems[] when possible

--- src/main.ts
+++ src/main.ts
@@ -58,53 +58,63 @@ const upgradeTimesText = document.createElement("p");
 document.body.appendChild(upgradeTimesText);
 upgradeTimesText.textContent = `__Upgrades__`;
 
-//Upgrade bonanza
+//Upgrade text elements
 const upgrades: HTMLButtonElement[] = [];
-const upgradeCosts: number[] = [10, 100, 1000];
 const upgradeTimes: number[] = [0, 0, 0];
-const upgradeValues: number[] = [0.1, 2.0, 50.0];
 const upgradeTexts: HTMLParagraphElement[] = [];
-const upgradeNames: string[] = ["Clowns", "Ball Pits", "Ball Factory"];
+
+//Replace the upgrade consts with an interface
+interface Item {
+  name: string;
+  cost: number;
+  rate: number;
+}
+
+const availableItems: Item[] = [
+  { name: "Clowns", cost: 10, rate: 0.1 },
+  { name: "Ball Pits", cost: 100, rate: 2 },
+  { name: "Ball Factory", cost: 1000, rate: 50 },
+];
 
 //Three upgrade texts
 for (let i = 0; i < 3; i++) {
   upgradeTexts[i] = document.createElement("p");
   document.body.appendChild(upgradeTexts[i]);
-  upgradeTexts[i].textContent = `${upgradeNames[i]}: ${upgradeTimes[i]} times`;
+  upgradeTexts[i].textContent = `${availableItems[i].name}: ${
+    upgradeTimes[i]
+  } times`;
 }
 
-//Create the upgrade button three times
-for (let i = 0; i < 3; i++) {
+//Create the upgrade buttons
+for (let i = 0; i < availableItems.length; i++) {
   upgrades[i] = document.createElement("button");
   document.body.appendChild(document.createElement("br"));
   document.body.appendChild(document.createElement("br"));
   document.body.appendChild(upgrades[i]);
-  upgradeCosts[i] = Math.pow(10, i) * 10; //10, 100, 1000
-  upgrades[i].textContent = `${upgradeNames[i]}: +${
-    upgradeValues[i]
-  } balls/sec (Cost: ${upgradeCosts[i]})`;
+  upgrades[i].textContent = `${availableItems[i].name}: +${
+    availableItems[i].rate
+  } balls/sec (Cost: ${availableItems[i].cost.toFixed(2)})`;
 
-  //Disable the button
   upgrades[i].disabled = true;
 
   upgrades[i].onclick = () => {
-    if (redBallClicks >= upgradeCosts[i]) {
-      redBallClicks -= upgradeCosts[i];
+    if (redBallClicks >= availableItems[i].cost) {
+      redBallClicks -= availableItems[i].cost;
 
-      growthRate += upgradeValues[i];
+      growthRate += availableItems[i].rate;
       delay = 1000 / growthRate;
 
       setInterval(IncrementRedBall, delay);
 
       upgradeTimes[i] = (upgradeTimes[i] || 0) + 1;
-      upgradeCosts[i] *= 1.15;
-      upgrades[i].textContent = `${upgradeNames[i]}: +${
-        upgradeValues[i]
-      } balls/sec (Cost: ${upgradeCosts[i]})`;
+      availableItems[i].cost *= 1.15;
+      upgrades[i].textContent = `${availableItems[i].name}: +${
+        availableItems[i].rate
+      } balls/sec (Cost: ${availableItems[i].cost.toFixed(2)})`;
       growthText.textContent = `Growth Rate: ${1 / (delay * 0.001)} balls/sec`;
       UpdateUpgradeTimesText();
 
-      if (redBallClicks < upgradeCosts[i]) {
+      if (redBallClicks < availableItems[i].cost) {
         upgrades[i].disabled = true;
       }
     }
@@ -122,7 +132,7 @@ function IncrementRedBall(): void {
 
   //Enable upgrade buttons
   for (let i = 0; i < upgrades.length; i++) {
-    if (redBallClicks >= upgradeCosts[i]) {
+    if (redBallClicks >= availableItems[i].cost) {
       upgrades[i].disabled = false;
     }
   }
@@ -131,7 +141,7 @@ function IncrementRedBall(): void {
 //Helper function for Update upgrade times text
 function UpdateUpgradeTimesText(): void {
   for (let i = 0; i < upgradeTimes.length; i++) {
-    upgradeTexts[i].textContent = `${upgradeNames[i]}: ${
+    upgradeTexts[i].textContent = `${availableItems[i].name}: ${
       upgradeTimes[i]
     } times`;
   }
